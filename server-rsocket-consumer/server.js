c4f79ee76b9c9aa7ded118dc91b42e60a8763d76
Kevin Viglucci <kviglucci@gmail.com>
2020-01-22 02:54:38 -0600

    add eslint support + add metric aggregation in publisher and consumer

--- server-rsocket-consumer/server.js
+++ server-rsocket-consumer/server.js
@@ -10,14 +10,16 @@ consoleDecorator(console, {
 const HTTP_PORT = process.env.NODE_HTTP_PORT;
 
 throng({
-  workers: 1,
-  master: () => {
-    console.log('Starting master process');
-  },
-  start: async (id) => {
-    const getApps = require('./app');
-    const [netifiGateway, httpServer] = await getApps();
-    httpServer.listen(HTTP_PORT, () => console.log(`Child process #${id} listening on HTTP port ${HTTP_PORT}`));
-    netifiGateway._connect();
-  }
+    workers: 1,
+    master: () => {
+        console.log('Starting master process');
+    },
+    start: async (id) => {
+        const getApps = require('./app');
+        const [netifiGateway, httpServer] = await getApps();
+        httpServer.listen(HTTP_PORT, () => {
+            return console.log(`Child process #${id} listening on HTTP port ${HTTP_PORT}`)
+        });
+        netifiGateway._connect();
+    }
 });
--- server-rsocket-publisher/app.js
+++ server-rsocket-publisher/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const requestLoggerFactory = require('morgan');
 const WebSocket = require('ws');
-const {Netifi} = require('netifi-js-client');
-const {HelloServiceServer} = require('./generated/rsocket/services_rsocket_pb');
-const {DefaultHelloService} = require('./services/DefaultHelloService');
+const { Netifi } = require('netifi-js-client');
+const { HelloServiceServer } = require('./generated/rsocket/services_rsocket_pb');
+const { DefaultHelloService } = require('./services/DefaultHelloService');
+const {
+    SimpleMeterRegistry,
+    MetricsSnapshotHandlerClient,
+    MetricsExporter
+} = require('rsocket-rpc-metrics');
 
 const groupName = 'netifi-rsocket-js-example.servers';
 const netifiGateway = Netifi.create({
@@ -21,15 +26,31 @@ const netifiGateway = Netifi.create({
     }
 });
 
+const meterRegistry = new SimpleMeterRegistry();
+
+const metricsClient = new MetricsSnapshotHandlerClient(
+    netifiGateway.group('com.netifi.broker.metrics')
+);
+
+const metricsExporter = new MetricsExporter(
+    metricsClient,
+    meterRegistry,
+    5, // 5 seconds
+    1024,
+);
+
+metricsExporter.start();
+
 const helloService = new DefaultHelloService();
-const helloServiceServer = new HelloServiceServer(helloService);
-netifiGateway.addService("com.viglucci.netifi.rsocket.js.example.service.HelloService", helloServiceServer);
+const helloServiceServer = new HelloServiceServer(helloService, undefined, meterRegistry);
+
+netifiGateway.addService('com.viglucci.netifi.rsocket.js.example.service.HelloService', helloServiceServer);
 
 const httpApp = express();
 
 httpApp.use(requestLoggerFactory('combined'));
 
-httpApp.get('/', (req, res, next) => {
+httpApp.get('/', (req, res) => {
     res.send('server-rsocket-publisher');
 });
 
